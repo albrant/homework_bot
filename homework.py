eee7d65da3b597a474109b651df8e84801953382
Evgeniy Albrant <skizee@yandex.ru>
2022-01-14 00:04:40 +0700

    Just begin
    Только начал делать. Ещё ничего не работает, но нужно залить на гит, чтобы можно было дальше работать с любого места

--- homework.py
+++ homework.py
@@ -1,16 +1,26 @@
-...
+import logging
+import os
+import time
+
+import requests
+from telegram import Bot
+from dotenv import load_dotenv
+from telegram.ext import CommandHandler, Updater
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('TOKEN_PRAKTIKUM')
+TELEGRAM_TOKEN = os.getenv('TOKEN_TELEGRAM')
+TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
 
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -20,7 +30,8 @@ HOMEWORK_STATUSES = {
 
 
 def send_message(bot, message):
-    ...
+    bot.send_message(TELEGRAM_CHAT_ID, message)
+    logging.info(f'Сообщение отправлено: {message}')
 
 
 def get_api_answer(current_timestamp):
@@ -31,8 +42,7 @@ def get_api_answer(current_timestamp):
 
 
 def check_response(response):
-
-    ...
+    return response.json()['homeworks']
 
 
 def parse_status(homework):
@@ -49,31 +59,39 @@ def parse_status(homework):
 
 
 def check_tokens():
-    ...
+    return True
 
 
 def main():
     """Основная логика работы бота."""
 
-    ...
+    if not(check_tokens()):
+        logging.error(f'Не получилось считать переменные окружения')
+        exit
+        
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
 
     ...
 
     while True:
         try:
-            response = ...
+            response = requests.get(ENDPOINT)
 
-            ...
+            homeworks = check_response(response)
+            
+            if len(homeworks) > 0:
+                status_key = parse_status(homeworks[-1])
+                send_message(bot, HOMEWORK_STATUSES[status_key])
+                
 
-            current_timestamp = ...
+            current_timestamp = int(time.time())
             time.sleep(RETRY_TIME)
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            logging.error(message)
             time.sleep(RETRY_TIME)
         else:
             ...
