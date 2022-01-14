ac99bb1b747b5603839ea3f466d5bddd8a62c46f
Evgeniy Albrant <skizee@yandex.ru>
2022-01-14 13:39:28 +0700

    Second commit
    Второй раз закомиттил. В целом что-то работает, но Pytest не проходит

--- homework.py
+++ homework.py
@@ -5,7 +5,6 @@ import time
 import requests
 from telegram import Bot
 from dotenv import load_dotenv
-from telegram.ext import CommandHandler, Updater
 
 load_dotenv()
 
@@ -17,6 +16,7 @@ TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -32,33 +32,45 @@ HOMEWORK_STATUSES = {
 def send_message(bot, message):
     bot.send_message(TELEGRAM_CHAT_ID, message)
     logging.info(f'Сообщение отправлено: {message}')
+    # доработать проверку на сбой при отправке сообщения
 
 
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    homeworks = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    return homeworks.json()
 
 
 def check_response(response):
-    return response.json()['homeworks']
+    if response.status_code == 200:
+        answer = response.json().get('homeworks')
+        if len(answer) == 0:
+            logging.info(f'При парсинге ответа получен пустой словарь')
+        return answer
+    else:
+        raise Exception('Проблемы с API практикума')
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-
-    ...
-
-    verdict = ...
-
-    ...
-
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    try:
+        homework_name = homework.get('homework_name')
+        homework_status = homework.get('status')
+        if homework_status in HOMEWORK_STATUSES:
+            verdict = HOMEWORK_STATUSES[homework_status]
+            return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+        else:
+            message = (f'Получен неизвестный статус - {homework_status}')
+            logging.error(message)
+            return message
+            
+    except Exception as error:
+        logging.error(f'При парсинге ответа не обнаружены нужные ключи')
 
 
 def check_tokens():
+    if PRACTICUM_TOKEN is None or TELEGRAM_TOKEN is None or TELEGRAM_CHAT_ID is None:
+        return False
     return True
 
 
@@ -66,26 +78,23 @@ def main():
     """Основная логика работы бота."""
 
     if not(check_tokens()):
-        logging.error(f'Не получилось считать переменные окружения')
+        logging.critical(f'Не получилось считать переменные окружения')
         exit
         
 
     bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-
-    ...
+    #send_message(bot, 'Приложение запущено')
 
     while True:
         try:
-            response = requests.get(ENDPOINT)
-
-            homeworks = check_response(response)
-            
-            if len(homeworks) > 0:
-                status_key = parse_status(homeworks[-1])
-                send_message(bot, HOMEWORK_STATUSES[status_key])
-                
-
+            api_answer = get_api_answer(current_timestamp - 25*24*60*60) 
+            # что делать с теми статусами, которые уже были отправлены?
+            homeworks = check_response(api_answer)
+            print(homeworks)
+            for homework in homeworks:
+                verdict_message = parse_status(homework)
+                send_message(bot, verdict_message)
             current_timestamp = int(time.time())
             time.sleep(RETRY_TIME)
 
@@ -94,7 +103,7 @@ def main():
             logging.error(message)
             time.sleep(RETRY_TIME)
         else:
-            ...
+            pass
 
 
 if __name__ == '__main__':
